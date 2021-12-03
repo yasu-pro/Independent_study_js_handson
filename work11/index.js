5a5daecda9446292c127f492274356fb06c6d898
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-12-04 08:53:26 +0900

    create work11

---
+++ work11/index.js
@@ -0,0 +1,62 @@
+"use strict";
+const wrap = document.getElementById("js-wrap")
+const ul = document.getElementById("js-list");
+const url = "http://myjson.dit.upm.es/api/bins/ajy3"
+
+function getData() {
+  const result = new Promise((resolve, reject) => {
+    resolve(
+      async function () {
+        const response = (await fetch(url)).json();
+        return response
+      }()
+    )
+  })
+  return result;
+}
+
+async function displayView() {
+  try {
+    const arrayData = await getData();
+    createElement(arrayData);
+  } catch (e) {
+    wrap.textContent = e.message;
+  } finally {
+    hideLoading();
+  }
+}
+
+function hideLoading() {
+  ul.style.backgroundImage = "none";
+}
+
+function createElement(imgArray) {
+  const fragment = document.createDocumentFragment();
+
+  Object.keys(imgArray).forEach(key => {
+
+    imgArray[key].forEach(keyIndex => {
+
+      const li = document.createElement("li");
+      const a = document.createElement("a");
+      const img = document.createElement("img");
+
+      a.href = keyIndex.a;
+      a.textContent = keyIndex.text;
+      img.src = keyIndex.img;
+      img.alt = keyIndex.alt;
+      fragment.appendChild(li).appendChild(a).prepend(img);
+    });
+  })
+  ul.appendChild(fragment);
+}
+
+function loading() {
+  ul.style.backgroundImage = "url(./img/loading-circle.gif)";
+  ul.style.backgroundRepeat = "no-repeat";
+  ul.style.height = "100px";
+}
+
+
+loading();
+displayView();
