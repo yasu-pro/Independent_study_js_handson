0ec3f9d99badbdb61ac5096147a5561843bacd1d
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-12-23 07:47:28 +0900

    feat Get data from JSON

--- work16/index.js
+++ work16/index.js
@@ -1,1 +1,44 @@
 "use strict";
+const tabs = document.getElementById("tabs");
+const fragment = document.createDocumentFragment();
+
+function getNewsData() {
+  const promiseNewsData = new Promise((resolve, reject) => {
+    resolve(
+      (async function () {
+        const result = (
+          await fetch("http://myjson.dit.upm.es/api/bins/c2fh")
+        ).json();
+        return result;
+      })()
+    );
+  });
+  return promiseNewsData;
+}
+
+async function hoge() {
+  const newsDataJSON = await getNewsData();
+  console.log(newsDataJSON.data[0].field);
+}
+
+hoge();
+// console.log(newsDataJSON.length);
+// console.log(JSON.parse(newsDataJSON));
+// console.log(newsDataJSON.data["img"]);
+
+// for (let i in newsDataJSON) {
+//   if (newsDataJSON.hasOwnProperty(i)) {
+//     let element = newsDataJSON[i].field;
+//     console.log(element);
+//   }
+// }
+
+// 取得したjsondata分だけタブを生成
+//ul>li(tab)>li()記事
+
+for (let i = 0; i < 5; i++) {
+  const tab = document.createElement("li");
+  fragment.appendChild(tab);
+  tab.className = "tab";
+}
+tabs.appendChild(fragment);
