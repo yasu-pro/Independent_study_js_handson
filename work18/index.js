23ef081d02a815198d0275b1f73bf56d97af185d
yasu-pro <cb1-y.s@docomo.ne.jp>
2024-04-04 18:01:01 +0900

    Added functions for adding images and creating next and prev buttons

--- work18/index.js
+++ work18/index.js
@@ -13,20 +13,51 @@ const getSlideImage = async () => {
     return slideData;
 };
 
+const addImage = (slide) => {
+    // 画像追加
+    const lists = document.createElement('li');
+    const img = document.createElement('img');
+
+    lists.id = slide.id;
+    img.src = slide.src;
+    lists.style.zIndex = `-${slide.order}`;
+
+    lists.appendChild(img);
+    sliderList.appendChild(lists);
+};
+
 const displaySlideImage = async () => {
     const slideData = await getSlideImage();
 
-    slideData.forEach((slide) => {
-        const lists = document.createElement('li');
-        const img = document.createElement('img');
-
-        lists.id = slide.id;
-        img.src = slide.src;
-        lists.style.zIndex = `-${slide.order}`;
-
-        lists.appendChild(img);
-        sliderList.appendChild(lists);
-    });
+    slideData.forEach(addImage);
 };
 
+const createNextPrevBtn = () => {
+    // next,prevボタン作成
+    const div = document.createElement('div');
+    const nextBtn = document.createElement('button');
+    const prevBtn = document.createElement('button');
+
+    div.classList = 'nextPrevBtnWrap';
+    nextBtn.id = 'nextBtn';
+    prevBtn.id = 'prevBtn';
+    nextBtn.classList = 'arrowBtn --nextBtn';
+    prevBtn.classList = 'arrowBtn --prevBtn';
+
+    nextBtn.textContent = '→';
+    prevBtn.textContent = '←';
+
+    div.appendChild(nextBtn);
+    div.appendChild(prevBtn);
+    slider.appendChild(div);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nextBtn = document.getElementById('nextBtn');
+    const prevBtn = document.getElementById('prevBtn');
+
+    console.log(nextBtn, prevBtn);
+});
+
 displaySlideImage();
+createNextPrevBtn();
