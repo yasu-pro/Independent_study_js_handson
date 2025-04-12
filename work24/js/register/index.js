6880888cc67745ce715f75174fff98e4e0da7d2c
yasu-pro <cb1-y.s@docomo.ne.jp>
2025-04-12 22:31:39 +0900

    利用規約モーダルの最後の規約までスクロールしたらclassを付与する

--- work24/js/register/index.js
+++ work24/js/register/index.js
@@ -1,5 +1,6 @@
 const registerElem = document.querySelector('.registerTxt');
 const closeBtn = document.querySelector('.closeBtn');
+const modalElem = document.querySelector('.modal_contents');
 
 registerElem.addEventListener('click', () => {
     const modalElem = document.getElementById('js-modal');
@@ -11,4 +12,24 @@ closeBtn.addEventListener('click', () => {
     const modalElem = document.getElementById('js-modal');
     modalElem.classList.remove('open');
     modalElem.classList.add('close');
+});
+
+modalElem.addEventListener('scroll', () => {
+    const registerElem = document.querySelectorAll('.modal li');
+
+    if (registerElem.length > 0) {
+        const lastElem = registerElem[registerElem.length - 1];
+        const scroll = modalElem.scrollTop;
+        const modalHeight = modalElem.clientHeight;
+        const lastElemPos =
+            lastElem.getBoundingClientRect().top -
+            modalElem.getBoundingClientRect().top;
+
+        console.log('scrollTop', scroll);
+        console.log('lastElemPos', lastElemPos);
+
+        if (lastElemPos < modalHeight) {
+            lastElem.classList.add('js-agree-position');
+        }
+    }
 });
