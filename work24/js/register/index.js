691d8451326e08b7fafdd2f1b736709c93556b73
yasu-pro <cb1-y.s@docomo.ne.jp>
2025-04-17 17:22:28 +0900

    submitボタンをdislabledに変更、バリデーションがすべてOKだったら送信ボタンを押せるようにする処理を追加

--- work24/js/register/index.js
+++ work24/js/register/index.js
@@ -3,6 +3,13 @@ const closeBtn = document.querySelector('.closeBtn');
 const modalElem = document.querySelector('.modal_contents');
 const registerCheckBox = document.getElementById('register');
 
+const validState = {
+    name: false,
+    mail: false,
+    password: false,
+    register: false,
+};
+
 registerTextElem.addEventListener('click', () => {
     const modalElem = document.getElementById('js-modal');
     modalElem.classList.remove('close');
@@ -18,7 +25,6 @@ closeBtn.addEventListener('click', () => {
 modalElem.addEventListener('scroll', () => {
     if (registerCheckBox.checked) return;
     const registerListElem = document.querySelectorAll('.modal li');
-    const submitBtn = document.getElementById('js-submitBtn');
 
     if (registerListElem.length > 0) {
         const lastElem = registerListElem[registerListElem.length - 1];
@@ -30,7 +36,9 @@ modalElem.addEventListener('scroll', () => {
         if (lastElemPos < modalHeight) {
             registerCheckBox.checked = true;
             registerCheckBox.disabled = false;
-            submitBtn.disabled = false;
+            validState.register = true;
+
+            toggleSubmit();
         }
     }
 });
@@ -40,10 +48,13 @@ inputUserName.addEventListener('keyup', () => {
     const invalidElem = document.querySelector('.invalidError.name');
     if (inputUserName.value.length > 15) {
         invalidElem.style.display = 'block';
-        return;
+        validState.name = false;
+    } else {
+        invalidElem.style.display = 'none';
+        validState.name = true;
     }
 
-    invalidElem.style.display = 'none';
+    toggleSubmit();
 });
 
 const inputMail = document.querySelector('input[name="mail"]');
@@ -54,10 +65,13 @@ inputMail.addEventListener('keyup', () => {
 
     if (!emailRegex.test(inputValue)) {
         invalidElem.style.display = 'block';
-        return;
+        validState.mail = false;
+    } else {
+        invalidElem.style.display = 'none';
+        validState.mail = true;
     }
 
-    invalidElem.style.display = 'none';
+    toggleSubmit();
 });
 
 const inputPassword = document.querySelector('input[name="password"]');
@@ -68,8 +82,20 @@ inputPassword.addEventListener('keyup', () => {
 
     if (!passwordRegex.test(password)) {
         invalidElem.style.display = 'block';
-        return;
+        validState.password = false;
+    } else {
+        invalidElem.style.display = 'none';
+        validState.password = true;
     }
 
-    invalidElem.style.display = 'none';
+    toggleSubmit();
 });
+
+const toggleSubmit = () => {
+    const submitBtn = document.getElementById('js-submitBtn');
+    const allValid = Object.values(validState).every(
+        (validValue) => validValue === true
+    );
+
+    submitBtn.disabled = !allValid;
+};
