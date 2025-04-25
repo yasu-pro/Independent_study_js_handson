d0d58be8b45e1be34627557bedcee83253ae7404
yasu-pro <cb1-y.s@docomo.ne.jp>
2025-04-25 17:47:28 +0900

    work27 first commit

---
+++ work27/src/login/js/mockServer.ts
@@ -0,0 +1,55 @@
+type User = {
+  userId: string;
+  name: string;
+  email: string;
+  password: string;
+};
+
+const findMatchingUser = (users, value: { id: string; password: string }) => {
+  return users.find((user) => {
+    return (
+      (user.name === value.id || user.email === value.id) &&
+      user.password === value.password
+    );
+  });
+};
+
+export const usersHandler = async (value: { id: string; password: string }) => {
+  try {
+    const res = await fetch(
+      "https://6802e9880a99cb7408eab082.mockapi.io/api/v1/users"
+    );
+    const users: User[] = await res.json();
+
+    if (users.length === 0) {
+      return {
+        ok: true,
+        code: 200,
+        message: "empty",
+        token: null,
+      };
+    }
+
+    const user = findMatchingUser(users, value);
+
+    if (user) {
+      return {
+        ok: true,
+        code: 200,
+        token: user.userId,
+      };
+    }
+
+    return {
+      ok: false,
+      code: 401,
+      message: "Not found",
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      code: 500,
+      message: "サーバーエラー",
+    };
+  }
+};
